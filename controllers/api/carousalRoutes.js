8a188546cdb37f38df3e23a553257968ed4c7d8e
konnenhannaford <konnen23@gmail.com>
2021-12-01 05:14:51 +1100

    updated movie info and main handlebars to allow for styling of main screen

--- controllers/api/carousalRoutes.js
+++ controllers/api/carousalRoutes.js
@@ -4,10 +4,17 @@ const router = require('express').Router();
 router.get('/',(req,res)=>{
     console.log("im hitting")
     const movies=[
-    {"title":"Hary Poop", "release_date":"10/10/20", "poster_path":"/img/Avatar.jpg"},
-    {"title":"Hary Poppy", "release_date":"10/10/20", "poster_path":"/img/Avatar.jpg"},
-    {"title":"Hary Poor", "release_date":"10/10/20", "poster_path":"/img/Avatar.jpg"},
-    {"title":"Hary potter", "release_date":"10/10/20", "poster_path":"/img/Avatar.jpg"}]
+    {"title":"Avatar 2", "release_date":"15 December 2022", "poster_path":"/img/Avatar.jpg"},
+    {"title":"Black Adam", "release_date":"29 July 2022<", "poster_path":"/img/Cow.jpg"},
+    {"title":"Cow", "release_date":"15 August 2022", "poster_path":"/img/BlackAdam.jpg"},
+    {"title":"Memory Box", "release_date":"15 August 2022", "poster_path":"/img/MemoryBox.jpg"},
+    {"title":"Spiderman", "release_date":"16 December 2021", "poster_path":"/img/SpiderMan.jpg"},
+    {"title":"The Drovers Wife", "release_date":"5 May 2022", "poster_path":"/img/TheDroversWife.jpg"},
+    {"title":"The Hill were Lioness roar", "release_date":"8 August 2022", "poster_path":"/img/TheHillWhereLionessesRoar.jpg"},
+    {"title":"The Scary of sixty First", "release_date":"13 August 2022", "poster_path":"/img/TheScaryofSixtyFirst.jpg"},
+    {"title":"Transform", "release_date":"9 December 2022", "poster_path":"/img/Transform.jpg"},
+    {"title":"Wicked Game", "release_date":"22 September 2022", "poster_path":"/img/WickedGame.jpg"}]
+
     
         res.render('carousal_homepage',{layout:false,movies})
   })
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 
 const router = require( "express" ).Router();
 
-//const apiRoutes = require( "./api");
+const apiRoutes = require( "./api");
 const homeRoutes = require( "./api/carousalRoutes" );
 
 router.use('/', homeRoutes);
-//router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
 router.use(( req, res ) => { res.send( 
     `<h1 style="display: table-cell; width: 100vw; height: 100vh; vertical-align:middle; text-align:center;"> 
