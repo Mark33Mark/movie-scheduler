c94e708f717a4cfccfb2701201438a4e393b133d
HobbZ <zachobba@gmail.com>
2021-11-30 18:45:37 +1000

    Updated homepage, upcoming single movie page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,25 +1,29 @@
 
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Upcoming, Watchlist, User } = require('../models');
 const withAuth = require('../utilities/auth');
 
 router.get('/', async (req, res) => {
     try {
-        // Get all projectes and JOIN with user data.
-        const savedProjects = await Project.findAll({
+        // Get all upcoming movies and JOIN with user data.
+        const upMovies = await Upcoming.findAll({
             include: [
                 { 
                     model: User, 
                     attributes:['name'],
                 },
+                {
+                    model: Upcoming,
+                    attributes: ['genre_id', 'title', 'release_date', 'overview', 'language', 'poster_path', 'popularity'],
+                  },
             ],            
         });
     
-    const projects = savedProjects.map( ( project ) => project.get({ plain: true }));
+    const upcomings = upMovies.map( ( upcoming ) => upcoming.get({ plain: true }));
 
 // Pass serialize data and session flag into template.
     res.render('homepage', { 
-        projects, 
+        upcomings, 
         logged_in: req.session.logged_in 
     });
 
@@ -28,7 +32,33 @@ router.get('/', async (req, res) => {
     }
 });
 
-
+//Get upcoming movie by id
+router.get('/upcoming/:id', async (req, res) => {
+    try {
+      const movieData = await Upcoming.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+  
+          },
+          {
+            model: Upcoming,
+            attributes: ['genre_id', 'title', 'release_date', 'overview', 'language', 'poster_path'],
+          },
+        ],
+      });
+  
+      const upcoming = movieData.get({ plain: true });
+  
+      res.render('upcoming', {
+        ...upcoming,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
 router.get('/login', (req, res) => {
